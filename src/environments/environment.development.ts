7e7acd9a874634401183f008c748a2915315ec7a
Mohamed Khairy <91643324+mohamedkhairy996@users.noreply.github.com>
2025-09-09 21:51:07 +0300

    Add files via upload

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    baseUrl : "https://localhost:7148/api",
+    hupURL:"https://localhost:7148",
+    stripePublicKey : "pk_test_51S3NueAVpXF56Emu8xkEGRFyek8szTNFrJGRXQorHFAH718egYjzLlD5kbvf0xpwOjNeJZyAU3yVniiyXm5oHewW00lnUE77KM",
+};
